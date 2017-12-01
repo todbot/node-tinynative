46dd19c3c83d09c9bb7230ffc504a30a310e8c88
Tod E. Kurt <tod@todbot.com>
2017-11-30 16:12:09 -0800

    added atexit code, but disabled

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 node-tinynative
 
-Smallest possible Node.js native module
+Smallest possible Node.js native module.
+Exposes a single function that prints to console
 
-
+To use:
 ```
 npm install tinynative
+```
+
+```
+var tinynative = require('tinynative');
+tinynative("hello there");  // prints out "tinynative: hello there"
 ```
 
 
 To develop:
 
 ```
-git clone tinynative
-cd tinynative
-node-gyp configure
-node-gyp build
+git clone https://github.com/todbot/node-tinynative
+cd node-tinynative
+npm install
+node-gyp rebuild
 ```
--- tinynative.cc
+++ tinynative.cc
@@ -1,16 +1,32 @@
 #include <nan.h>
 #include <stdio.h>
 
+#define USE_ATEXIT 0
+
 using namespace v8;
+using namespace node;
+
+#if USE_ATEXIT
+static void
+deinitialize(void*)
+{
+  printf("tinynative deinitialize\n");
+}
+#endif
 
 NAN_METHOD(Print) {
   if (!info[0]->IsString()) return Nan::ThrowError("Must pass a string");
   Nan::Utf8String path(info[0]);
-  printf("Hello from C++: %s\n", *path);
+  printf("tinynative: %s\n", *path);
 }
 
 NAN_MODULE_INIT(InitAll) {
+
   Nan::Set(target, Nan::New<String>("print").ToLocalChecked(), Nan::GetFunction(Nan::New<FunctionTemplate>(Print)).ToLocalChecked());
+
+#if USE_ATEXIT
+  node::AtExit(deinitialize);
+#endif
 }
 
 NODE_MODULE(tinynative, InitAll)
